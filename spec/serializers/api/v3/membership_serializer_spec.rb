92e6d2f2874c44219a5adfacb3feeb46b52e44fc
Maciej Czuchnowski <maciej.czuchnowski@netguru.co>
2016-02-29 11:46:09 +0100

    Introduce better naming

--- spec/serializers/api/v3/membership_serializer_spec.rb
+++ spec/serializers/api/v3/membership_serializer_spec.rb
@@ -4,10 +4,10 @@ describe Api::V3::MembershipSerializer do
   let(:project) { create(:project) }
   let(:role) { create(:role) }
   let(:object) { create(:membership, project: project, role: role) }
-  let(:hash) { described_class.new(object).serializable_hash }
+  let(:subject) { described_class.new(object).serializable_hash }
 
-  it { expect(hash[:project_name]).to eq(project.name) }
-  it { expect(hash[:role_name]).to eq(role.name) }
-  it { expect(hash[:potential]).to eq(project.potential) }
+  it { expect(subject[:project_name]).to eq(project.name) }
+  it { expect(subject[:role_name]).to eq(role.name) }
+  it { expect(subject[:potential]).to eq(project.potential) }
   include_examples 'attributes', %w(starts_at ends_at)
 end
--- spec/serializers/api/v3/user_with_memberships_serializer_spec.rb
+++ spec/serializers/api/v3/user_with_memberships_serializer_spec.rb
@@ -4,17 +4,17 @@ describe Api::V3::UserWithMembershipsSerializer do
   let!(:object) { create(:user, :junior) }
   let!(:project) { create(:project) }
   let!(:membership) { create(:membership, project: project, user: object) }
-  let(:hash) { described_class.new(object).serializable_hash }
+  let(:subject) { described_class.new(object).serializable_hash }
 
   include_examples 'attributes', %w(first_name last_name)
 
   describe '#primary_role_name' do
-    it { expect(hash[:primary_role_name]).to eq('junior') }
+    it { expect(subject[:primary_role_name]).to eq('junior') }
   end
 
   describe 'assosciations' do
     it 'returns membership with project name' do
-      expect(hash[:memberships][0][:project_name]).to eq(project.name)
+      expect(subject[:memberships][0][:project_name]).to eq(project.name)
     end
   end
 end
