ef892dffc977b99fadc8dd7ba24ffb4451cf7c3e
Maciej Czuchnowski <maciej.czuchnowski@netguru.co>
2016-03-01 16:57:25 +0100

    Unpend and fix filter specs

--- spec/features/filters_spec.rb
+++ spec/features/filters_spec.rb
@@ -4,7 +4,8 @@ describe 'Dashboard filters', js: true do
   let(:admin_user) { create(:user, :admin) }
   let(:dev_role) { create(:role, name: 'developer') }
   let(:role) { create(:role_billable) }
-  let!(:dev_position) { create(:position, user: dev_user, role: dev_role) }
+  let!(:dev_position) { create(:position, :primary, user: dev_user, role: dev_role) }
+  let!(:admin_dev_position) { create(:position, :primary, user: admin_user, role: dev_role) }
 
   let!(:dev_user) do
     create(:user, :admin, last_name: 'Developer', primary_role: dev_role,
@@ -26,13 +27,14 @@ describe 'Dashboard filters', js: true do
   end
 
   describe 'users filter' do
-    xit 'returns only matched projects when user name provided' do
-      select_option('users', 'Developer Daisy')
+    it 'returns only matched projects when user name provided' do
+      find('.filter.users .Select-control').click
+      find( 'div.Select-option', text: 'Developer Daisy' ).click
       expect(page).to have_text('test')
       expect(page).to_not have_text('zztop')
     end
 
-    xit 'returns all projects when no selectize provided' do
+    it 'returns all projects when no selectize provided' do
       expect(page).to have_text('zztop')
       expect(page).to have_text('test')
     end
@@ -40,36 +42,34 @@ describe 'Dashboard filters', js: true do
     context 'when user has not started a project' do
       let!(:junior_role) { create(:role, name: 'junior') }
       let!(:future_dev) { create(:user, primary_role: junior_role) }
-      let!(:future_membership) { create(:membership, user: future_dev, starts_at: 1.week.from_now) }
+      let!(:future_membership) { create(:membership, user: future_dev, starts_at: 1.week.from_now,
+        project: project_zztop) }
 
-      xit 'shows the user' do
+      it 'does not show the project' do
+
         full_name = "#{future_dev.last_name} #{future_dev.first_name}"
         visit '/dashboard'
-        select_option('users', full_name)
 
-        if page.has_css?('.invisible')
-          within '.invisible' do
-            expect(page).not_to have_text(full_name)
-          end
-        end
+        expect(page).to have_text('zztop')
+        find('.filter.users .Select-control').click
+        find( 'div.Select-option', text: full_name ).click
 
-        within '#projects-users' do
-          expect(page).to have_text(full_name)
-        end
+        expect(page).to_not have_text('zztop')
       end
     end
   end
 
   describe 'projects filter' do
-    xit 'shows all projects when empty string provided' do
+    it 'shows all projects when empty string provided' do
       within '#projects-users' do
         expect(page).to have_text('zztop')
         expect(page).to have_text('test')
       end
     end
 
-    xit 'shows only matched projects when project name provided' do
-      select_option 'projects', 'zztop'
+    it 'shows only matched projects when project name provided' do
+      find('.filter.projects .Select-control').click
+      find( 'div.Select-option', text: 'zztop' ).click
 
       within '#projects-users' do
         expect(page).to have_text('zztop')
