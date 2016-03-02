5e66c57ea027b2e8022b09a64e7ec18c5de8184f
Maciej Czuchnowski <maciej.czuchnowski@netguru.co>
2016-03-02 17:35:16 +0100

    Unpend and fix user_profile_spec

--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe 'profile', js: true do
-  let!(:junior) { create(:role, name: 'junior', priority: 2) }
-  let!(:developer) { create(:role, name: 'developer', priority: 1) }
-  let(:position) { create(:position, role: junior) }
+  let!(:junior_role) { create(:role, name: 'junior') }
+  let!(:developer_role) { create(:role, name: 'developer') }
+  let(:position) { create(:position, role: junior_role, primary: false) }
 
   before do
     page.set_rack_session 'warden.user.user.key' => User
@@ -13,30 +13,26 @@ describe 'profile', js: true do
   describe 'setting primary role' do
     before { visit user_path(position.user.id) }
 
-    xit 'set primary role to a user' do
-      expect(page).to have_select('user-primary', selected: 'no role')
-      select(position.role.name, from: 'user-primary')
+    it 'sets role as primary using a slider' do
+      find('.primary-slider').click
+      wait_for_ajax
 
-      within('form.edit_user') do
-        first('input[type=submit]').click
-      end
-
-      expect(page).to have_select('user-primary', selected: position.role.name)
+      expect(position.reload.primary).to be true
     end
   end
 
   describe 'adding positions' do
     before { visit user_path(position.user.id) }
 
-    xit 'adds a position to user' do
-      expect(page).to have_select('user-primary', options: ['no role', 'junior'])
-
-      within('form.edit_user') do
+    it 'adds a position to user' do
+      within('.user-positions') do
+        expect(page).to have_text(junior_role.name)
+        expect(page).to_not have_text(developer_role.name)
         click_link('Add position')
       end
 
       within('form.new_position') do
-        select(developer.name, from: 'position_role_id')
+        select(developer_role.name, from: 'position_role_id')
         select(
           "#{position.user.last_name} #{position.user.first_name}",
           from: 'position_user_id'
@@ -50,10 +46,7 @@ describe 'profile', js: true do
         click_button('Create Position')
       end
 
-      expect(page).to have_select(
-        'user-primary',
-        options: ['no role', 'junior', 'developer']
-      )
+      within('.user-positions') { expect(page).to have_text(developer_role.name) }
     end
   end
 
