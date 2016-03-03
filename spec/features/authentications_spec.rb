7030181539c0ae0dd5b8d3c50555f1f3ab7b5cfe
smnkrt <szymon.krywult@netguru.pl>
2016-03-03 11:42:10 +0100

    update authentications_spec

--- spec/features/authentications_spec.rb
+++ spec/features/authentications_spec.rb
@@ -1,25 +1,31 @@
 require 'spec_helper'
 
 describe 'Authentication', js: true do
-
   subject { page }
   before(:all) { OmniAuth.config.test_mode = true }
 
   context 'sign in' do
-    let(:user) { build(:user, first_name: 'John', last_name: 'Doe', email: 'jdoe@sth.com') }
+    let(:internal_domain) { AppConfig.emails.internal }
+    let(:user) do
+      build(:user, first_name: 'John', last_name: 'Doe', email: "jdoe@#{internal_domain}")
+    end
     before do
-      OmniAuth.config.add_mock(:google_oauth2,
-      info: {
-        first_name: user.first_name,
-        last_name: user.last_name,
-        email: user.email
-      },
-      extra: { raw_info: { hd: 'example.com' } },
-      credentials: {
-        oauth_token: 123,
-        refresh_token: 456,
-        oauth_expires_at: Time.now + 1.hour
-      })
+      OmniAuth.config.add_mock(
+        :google_oauth2,
+        {
+          info: {
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email
+          },
+          extra: { raw_info: { hd: internal_domain } },
+          credentials: {
+            oauth_token: 123,
+            refresh_token: 456,
+            oauth_expires_at: Time.now + 1.hour
+          }
+        }
+      )
     end
 
     context 'with google' do
@@ -28,8 +34,7 @@ describe 'Authentication', js: true do
         click_link_or_button 'Sign up with Google'
       end
 
-      # TODO fix specs after bug fixes
-      xit { should have_content('Now please connect your GitHub account.') }
+      it { should have_content('Now please connect your GitHub account.') }
 
       context 'and github account' do
         before do
@@ -37,8 +42,7 @@ describe 'Authentication', js: true do
           click_link_or_button 'connect'
         end
 
-        # TODO fix specs after bug fixes
-        xit 'redirects to the dashboard' do
+        it 'redirects to the dashboard' do
           expect(page).to have_content('Projects')
         end
       end
@@ -46,7 +50,16 @@ describe 'Authentication', js: true do
   end
 
   context 'sign in without GH account' do
-    let(:second_usr) { create(:user, first_name: 'Greg', gh_nick: nil, last_name: 'Doe', email: 'grdoe@sth.com', without_gh: true) }
+    let(:second_usr) do
+      create(
+        :user,
+        first_name: 'Greg',
+        gh_nick: nil,
+        last_name: 'Doe',
+        email: 'grdoe@sth.com',
+        without_gh: true
+      )
+    end
     before { sign_in(second_usr) }
 
     it 'redirects to the dashboard' do
