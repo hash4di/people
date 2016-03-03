125b4442d600aefc5fb4750cb7193f600be6b36e
smnkrt <szymon.krywult@netguru.pl>
2016-03-03 16:10:13 +0100

    unpend and update omniauth callbacks controller specs

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -5,27 +5,29 @@ describe OmniauthCallbacksController do
 
   describe '#google_oauth2' do
     let(:callback) { get :google_oauth2 }
+    let(:internal_domain) { AppConfig.emails.internal }
 
     before do
-      OmniAuth.config.add_mock(:google_oauth2, {
-        info: {
-          first_name: 'Jan',
-          last_name: 'Kowalski',
-          email: 'jan@kowalski.pl'
-        },
-        extra: { raw_info: { hd: 'example.com' }},
-        credentials: {
-          oauth_token: 123,
-          refresh_token: 456,
-          oauth_expires_at: Time.now+1.hour
+      OmniAuth.config.add_mock(
+        :google_oauth2, {
+          info: {
+            first_name: 'Jan',
+            last_name: 'Kowalski',
+            email: "jan@#{internal_domain}"
+          },
+          extra: { raw_info: { hd: internal_domain } },
+          credentials: {
+            oauth_token: 123,
+            refresh_token: 456,
+            oauth_expires_at: Time.now + 1.hour
+          }
         }
-      })
-      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+      )
+      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
     end
 
     context 'user is internal' do
-      # TODO fix specs after bug fixes
-      xit 'signs up user' do
+      it 'signs up user' do
         expect { callback }.to change { User.count }.by(1)
       end
 
@@ -42,7 +44,7 @@ describe OmniauthCallbacksController do
 
     context 'user is not internal' do
       before do
-        request.env["omniauth.auth"][:extra][:raw_info][:hd] = 'another_example.com'
+        request.env['omniauth.auth'][:extra][:raw_info][:hd] = 'another_example.com'
       end
 
       it "doesn't signs up user" do
@@ -66,8 +68,8 @@ describe OmniauthCallbacksController do
     let(:user) { create(:user, gh_nick: nil) }
 
     before do
-      OmniAuth.config.add_mock(:github, { info: { nickname: 'xyz' }})
-      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:github]
+      OmniAuth.config.add_mock(:github, { info: { nickname: 'xyz' } })
+      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
     end
 
     context 'user has signed up' do
