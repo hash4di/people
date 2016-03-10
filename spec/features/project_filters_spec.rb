5ab9a24004f8c6451a88972f2e6f3b92074cf0c7
Maciej Czuchnowski <maciej.czuchnowski@netguru.co>
2016-03-10 19:11:55 +0100

    Add roles filter spec

--- spec/features/project_filters_spec.rb
+++ spec/features/project_filters_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe 'Project dashboard filters', js: true do
   let!(:admin_user) { create(:user, :admin, :developer) }
   let!(:dev_user) { create(:user, :admin, :developer, last_name: 'Developer', first_name: 'Daisy') }
-  let!(:membership) { create(:membership, user: dev_user, project: project_test) }
   let!(:project_zztop) { create(:project, name: 'zztop') }
   let!(:project_test) { create(:project, name: 'test') }
+  let!(:membership) { create(:membership, user: dev_user, project: project_test) }
 
   let(:projects_page) { App.new.projects_page }
 
@@ -14,9 +14,25 @@ describe 'Project dashboard filters', js: true do
     projects_page.load
   end
 
+  describe 'roles filter' do
+    it 'shows all projects when empty string provided' do
+      expect(projects_page).to have_text('zztop')
+      expect(projects_page).to have_text('test')
+    end
+
+    it 'shows only matched projects when role is provided' do
+      react_select('.filter.roles', membership.role.name)
+
+      expect(projects_page).to_not have_text('zztop')
+      expect(projects_page).to have_text('test')
+    end
+  end
+
+
   describe 'users filter' do
     it 'returns only matched projects when user name provided' do
       react_select('.filter.users', 'Developer Daisy')
+
       expect(projects_page).to have_text('test')
       expect(projects_page).to_not have_text('zztop')
     end
@@ -35,6 +51,7 @@ describe 'Project dashboard filters', js: true do
 
       it 'does not show the project' do
         projects_page.project_types.active_tab.click
+
         expect(projects_page).to have_text('zztop')
         react_select('.filter.users', future_dev.decorate.name)
         expect(page).to_not have_text('zztop')
@@ -44,19 +61,15 @@ describe 'Project dashboard filters', js: true do
 
   describe 'projects filter' do
     it 'shows all projects when empty string provided' do
-      within '#projects-users' do
-        expect(projects_page).to have_text('zztop')
-        expect(projects_page).to have_text('test')
-      end
+      expect(projects_page).to have_text('zztop')
+      expect(projects_page).to have_text('test')
     end
 
     it 'shows only matched projects when project name provided' do
       react_select('.filter.projects', 'zztop')
 
-      within '#projects-users' do
-        expect(projects_page).to have_text('zztop')
-        expect(projects_page).not_to have_text('test')
-      end
+      expect(projects_page).to have_text('zztop')
+      expect(projects_page).not_to have_text('test')
     end
   end
 end
