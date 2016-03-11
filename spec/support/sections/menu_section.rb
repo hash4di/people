863c076deb80cd46cc8dba986f92a298c2e7ef34
Maciej Czuchnowski <maciej.czuchnowski@netguru.co>
2016-03-11 12:09:38 +0100

    Add menu elements

--- spec/support/sections/menu_section.rb
+++ spec/support/sections/menu_section.rb
@@ -1,3 +1,7 @@
 class MenuSection < SitePrism::Section
   element :logo, '.navbar-brand'
+  element :scheduling, 'ul.nav.navbar-nav li.scheduling'
+  element :users, 'ul.nav.navbar-nav li.users'
+  element :projects, 'ul.nav.navbar-nav li.dashboard'
+  element :teams, 'ul.nav.navbar-nav li.teams'
 end
