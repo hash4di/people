2615fa61a676bc7e237aa255a346fdb381ffa439
Maciej Czuchnowski <maciej.czuchnowski@netguru.co>
2016-03-15 21:51:32 +0100

    Remove backbone from configuration

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Hrguru
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.hamlcoffee.name_filter = lambda { |n| n.sub /^backbone\/templates\//, '' }
+
     config.action_dispatch.tld_length = AppConfig.tld_length
   end
 end
