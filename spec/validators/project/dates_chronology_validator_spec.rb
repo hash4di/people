202d61fd7691e9dc915e4e34883109d6a20fb06b
smnkrt <szymon.krywult@netguru.pl>
2016-04-05 08:35:06 +0200

    add Project::DatesChronologyValidator

---
+++ spec/validators/project/dates_chronology_validator_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Project::DatesChronologyValidator do
+  context 'record is valid' do
+    let(:record) { build(:project) }
+    it 'validates record and not raise errors' do
+      described_class.new.validate(record)
+      expect(record.errors).to be_empty
+    end
+  end
+
+  context 'record is invalid' do
+    let(:record) { build(:project, starts_at: Date.current, end_at: 2.days.ago) }
+    it 'validates record and not raise errors' do
+      described_class.new.validate(record)
+      expect(record.errors).to_not be_empty
+    end
+  end
+end
