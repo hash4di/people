c2eb9fec8a0e2c725de6b1ddcb2e3fbfdac37c6e
smnkrt <szymon.krywult@netguru.pl>
2016-05-13 11:59:28 +0200

    update specs

--- spec/services/projects/end_current_memberships_spec.rb
+++ spec/services/projects/end_current_memberships_spec.rb
@@ -1,19 +1,20 @@
 require 'spec_helper'
 
 describe Projects::EndCurrentMemberships do
-  let(:memberships) { [create(:membership, :without_end), create(:membership, :without_end)] }
+  let(:memberships) { [create(:membership, :without_end), create(:membership)] }
   let(:project) { create(:project, memberships: memberships) }
   subject { described_class.new(project) }
+  let(:time) { Date.current.end_of_day }
 
   before do
     subject.call
   end
 
   describe '#call' do
-    it 'sets' do
+    it 'sets ends_at for associated memberships' do
       aggregate_failures do
         memberships.each do |membership|
-          expect(membership.reload.ends_at).to_not be nil
+          expect(membership.reload.ends_at.to_i).to eq time.to_i
         end
       end
     end
