0b947bb696cc0c1bd84c6f2ce8b42ce6556fb303
Piotr Pawlik <piotr.pawlik@netguru.pl>
2016-10-24 10:43:01 +0200

    Added booked to membership serializer

--- app/serializers/api/v2/membership_serializer.rb
+++ app/serializers/api/v2/membership_serializer.rb
@@ -1,6 +1,6 @@
 module Api::V2
   class MembershipSerializer < ActiveModel::Serializer
-    attributes :project_name, :starts_at, :ends_at, :role, :project_id, :potential_project
+    attributes :project_name, :starts_at, :ends_at, :role, :project_id, :potential_project, :booked
 
     def project_name
       object.project.name
