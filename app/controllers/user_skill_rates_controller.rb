a519f0a32f7fa1b546491560919a5a6646ae0199
Dariusz Musielak <dariusz.musielak@netguru.pl>
2016-12-09 12:15:00 +0100

    Added user skill rate controller

---
+++ app/controllers/user_skill_rates_controller.rb
@@ -0,0 +1,50 @@
+class UserSkillRatesController < ApplicationController
+  before_action :set_skill, only: [:show, :edit, :update, :destroy]
+  before_action :set_grouped_skills, only: [:index]
+
+
+  def index
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @grouped_skills_by_category }
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user_skill_rate.update(user_skill_rate_params)
+        format.html { redirect_to @user_skill_rate, notice: 'Skill was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @user_skill_rate.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_skill
+    @user_skill_rate = Skill.find(params[:id])
+  end
+
+  def set_grouped_skills
+    @user_skill_rates = UserSkillRate.joins(
+      skill: :skill_category
+    ).select(
+      "
+        user_skill_rates.*,
+        skills.name as name,
+        skills.description as description,
+        skill_categories.name as category
+      "
+    ).where(user_id: current_user.id)
+    @grouped_skills_by_category = @user_skill_rates.group_by do |skill|
+      skill.category
+    end
+  end
+
+  def user_skill_rate_params
+    params.require(:user_skill_rate).permit(:rate, :note, :favorite)
+  end
+end
