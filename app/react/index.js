3749db63c838debc0c4fa870735ecfc51932a385
Dariusz Musielak <dariusz.musielak@netguru.pl>
2016-12-12 12:32:44 +0100

    Register new component

--- app/react/index.js
+++ app/react/index.js
@@ -7,10 +7,12 @@ import Statistics from './components/statistics/statistics';
 import Teams from './components/teams/teams';
 import Projects from './components/projects/projects';
 import Users from './components/users/users';
+import UserSkillRates from './components/skills/user_skill_rates';
 import Scheduling from './components/scheduling/scheduling';
 
 registerComponent('statistics', Statistics);
 registerComponent('teams', Teams);
 registerComponent('projects', Projects);
 registerComponent('users', Users);
+registerComponent('user_skill_rates', UserSkillRates);
 registerComponent('scheduling', Scheduling);
