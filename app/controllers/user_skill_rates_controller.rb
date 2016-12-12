63f92a9535b8160a3c2d811133e53e396c4f6204
Dariusz Musielak <dariusz.musielak@netguru.pl>
2016-12-12 18:36:47 +0100

    Use expose for user skill rates

--- app/controllers/user_skill_rates_controller.rb
+++ app/controllers/user_skill_rates_controller.rb
@@ -1,35 +1,33 @@
 class UserSkillRatesController < ApplicationController
-  before_action :set_user_skill_rate, only: [:show, :edit, :update, :destroy]
-  before_action :set_grouped_skills, only: [:index]
-
+  expose(:user_skill_rate) { UserSkillRate.find(params[:id]) }
+  expose(:user_skill_rates) { set_user_skill_rates }
+  expose(:grouped_skills_by_category) do
+    user_skill_rates.group_by { |skill| skill.category }
+  end
 
   def index
     respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @grouped_skills_by_category }
+      format.html
+      format.json { render json: grouped_skills_by_category }
     end
   end
 
   def update
     respond_to do |format|
-      if @user_skill_rate.update(user_skill_rate_params)
-        format.html { redirect_to @user_skill_rate, notice: 'Skill was successfully updated.' }
+      if user_skill_rate.update(user_skill_rate_params)
+        format.html { redirect_to user_skill_rate, notice: 'Skill was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @user_skill_rate.errors, status: :unprocessable_entity }
+        format.json { render json: user_skill_rate.errors, status: :unprocessable_entity }
       end
     end
   end
 
   private
 
-  def set_user_skill_rate
-    @user_skill_rate = UserSkillRate.find(params[:id])
-  end
-
-  def set_grouped_skills
-    @user_skill_rates = UserSkillRate.joins(
+  def set_user_skill_rates
+    user_skill_rates ||= UserSkillRate.joins(
       skill: :skill_category
     ).select(
       "
@@ -40,9 +38,6 @@ class UserSkillRatesController < ApplicationController
         skill_categories.name as category
       "
     ).where(user_id: current_user.id)
-    @grouped_skills_by_category = @user_skill_rates.group_by do |skill|
-      skill.category
-    end
   end
 
   def user_skill_rate_params
