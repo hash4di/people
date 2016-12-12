5a6850ebc5c046de129084a7b6cd9f6581317398
Dariusz Musielak <dariusz.musielak@netguru.pl>
2016-12-12 12:32:52 +0100

    Added source for user skill rate

---
+++ app/react/sources/UserSkillRateSource.js
@@ -0,0 +1,13 @@
+export default class UserSkillRateSource {
+  static update(params) {
+    debugger
+    return $.ajax({
+      url: Routes.user_skill_rate_path(params.id),
+      type: "PATCH",
+      dataType: 'json',
+      data: {
+        user_skill_rate: params
+      }
+    });
+  }
+}
