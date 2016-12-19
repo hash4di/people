02397249ea20c98b116058949ec3c329eab4dd85
Dariusz Musielak <dariusz.musielak@netguru.pl>
2016-12-19 19:02:07 +0100

    Updated specs for user skill rate model

--- spec/models/user_skill_rate_spec.rb
+++ spec/models/user_skill_rate_spec.rb
@@ -13,25 +13,18 @@ describe UserSkillRate do
 
   describe '#content' do
     let(:user_skill_rate) { create(:user_skill_rate) }
+    let!(:user_skill_rate_content_1) do
+      create(:user_skill_rate_content, user_skill_rate: user_skill_rate, rate: 1)
+    end
+    let!(:user_skill_rate_content_2) do
+      create(:user_skill_rate_content, user_skill_rate: user_skill_rate, rate: 2)
+    end
+    let!(:newest_user_skill_rate_content) do
+      create(:user_skill_rate_content, user_skill_rate: user_skill_rate, rate: 0)
+    end
 
     it 'returns newest content' do
-      create(:user_skill_rate_content, user_skill_rate: user_skill_rate, rate: 1)
-      create(:user_skill_rate_content, user_skill_rate: user_skill_rate, rate: 2)
-      content = create(:user_skill_rate_content, user_skill_rate: user_skill_rate, rate: 0)
-
-      result = user_skill_rate.content
-      expect(result).to eq(content)
-    end
-  end
-
-  describe '#rate' do
-    let(:user_skill_rate) { create(:user_skill_rate) }
-
-    it 'returns content#rate' do
-      create(:user_skill_rate_content, user_skill_rate: user_skill_rate, rate: 1)
-
-      result = user_skill_rate.rate
-      expect(result).to eq(1)
+      expect(user_skill_rate.content).to eq(newest_user_skill_rate_content)
     end
   end
 end
