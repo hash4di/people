192627404afd1929f12c99664a83ed96dbb37c2b
Dariusz Musielak <dariusz.musielak@netguru.pl>
2016-12-19 16:50:23 +0100

    Modified service for creating user skill rate

--- app/services/skills/user_skill_rates/create.rb
+++ app/services/skills/user_skill_rates/create.rb
@@ -1,21 +1,26 @@
 module Skills
   module UserSkillRates
     class Create
-      def initialize(user_id:, skill_id:, rate:)
+      def initialize(user_id:, skill_id:)
         @user_id = user_id
         @skill_id = skill_id
-        @rate = rate
       end
 
       def call
-        user_skill_rate = ::UserSkillRate.create(user_id: user_id, skill_id: skill_id)
-        user_skill_rate.contents.create(rate: rate)
-        user_skill_rate
+        user_skill_rate.contents.create(user_skill_rate_content_params)
       end
 
       private
 
-      attr_reader :user_id, :skill_id, :rate
+      def user_skill_rate
+        @user_skill_rate ||= ::UserSkillRate.create(user_id: user_id, skill_id: skill_id)
+      end
+
+      def user_skill_rate_content_params
+        user_skill_rate.attributes.slice('rate', 'note', 'favorite')
+      end
+
+      attr_reader :user_id, :skill_id
     end
   end
 end
