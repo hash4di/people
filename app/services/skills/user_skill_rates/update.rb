f8e02313f0d69858deee7b0505d8fb49a1e84dac
Dariusz Musielak <dariusz.musielak@netguru.pl>
2016-12-21 18:20:03 +0100

    check if user skill rate content exists and update it if yes, if no create new one

--- app/services/skills/user_skill_rates/update.rb
+++ app/services/skills/user_skill_rates/update.rb
@@ -8,7 +8,7 @@ module Skills
 
       def call
         update_user_skill_rate
-        create_new_user_skill_rate_content
+        create_or_update_user_skill_rate_content
         user_skill_rate
       end
 
@@ -17,15 +17,30 @@ module Skills
       attr_reader :user_skill_rate_id, :params
 
       def update_user_skill_rate
-        user_skill_rate.update_attributes(params)
+        user_skill_rate.update_attributes(update_params)
       end
 
-      def create_new_user_skill_rate_content
-        user_skill_rate.contents.create(params)
+      def create_or_update_user_skill_rate_content
+        if user_skill_rate_content
+          binding.pry
+          user_skill_rate_content.update_attributes(update_params)
+        else
+          user_skill_rate.contents.create(params)
+        end
+      end
+
+      def user_skill_rate_content
+        @user_skill_rate_content ||= user_skill_rate.contents.where(
+          "created_at > ?", Time.zone.now.beginning_of_day
+        ).last
       end
 
       def user_skill_rate
         @user_skill_rate ||= ::UserSkillRate.find(user_skill_rate_id)
+      end
+
+      def update_params
+        @update_params ||= params.slice(:note, :rate, :favorite)
       end
     end
   end
