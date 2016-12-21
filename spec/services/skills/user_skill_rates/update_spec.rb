05557f244cb2eb0becd5f2babdf98e1f88e7024d
Dariusz Musielak <dariusz.musielak@netguru.pl>
2016-12-21 18:20:26 +0100

    Updated specs for new version of user skill rate update service

--- spec/services/skills/user_skill_rates/update_spec.rb
+++ spec/services/skills/user_skill_rates/update_spec.rb
@@ -35,17 +35,43 @@ describe ::Skills::UserSkillRates::Update do
       expect{ subject.call }.to change{ user_skill_rate.reload.favorite }.from(false).to(true)
     end
 
-    it 'creates new user_skill_rate_content' do
-      expect{ subject.call }.to change{ user_skill_rate.contents.count }
+    context "when user_skill_rate_content doesn't exist today" do
+      it 'creates new user_skill_rate_content' do
+        expect{ subject.call }.to change{ user_skill_rate.contents.count }
+      end
+
+      it 'sets correct values on new content' do
+        subject.call
+        user_skill_rate.reload
+        aggregate_failures do
+          expect(last_content.favorite).to eq(user_skill_rate.favorite)
+          expect(last_content.note).to eq(user_skill_rate.note)
+          expect(last_content.rate).to eq(user_skill_rate.rate)
+        end
+      end
     end
 
-    it 'sets correct values on new content' do
-      subject.call
-      user_skill_rate.reload
-      aggregate_failures do
-        expect(last_content.favorite).to eq(user_skill_rate.favorite)
-        expect(last_content.note).to eq(user_skill_rate.note)
-        expect(last_content.rate).to eq(user_skill_rate.rate)
+    context 'when user_skill_rate_content exists today' do
+      let!(:user_skill_rate_content) do
+        create(:user_skill_rate_content,
+          user_skill_rate_id: user_skill_rate.id
+        )
+      end
+
+      it "doesn't create new user_skill_rate_content" do
+        expect{ subject.call }.to_not change{ user_skill_rate.contents.count }
+      end
+
+      it 'updates content' do
+        subject.call
+        user_skill_rate_content.reload
+        user_skill_rate.reload
+        aggregate_failures do
+          expect(user_skill_rate_content.favorite)
+            .to eq(user_skill_rate.favorite)
+          expect(user_skill_rate_content.note).to eq(user_skill_rate.note)
+          expect(user_skill_rate_content.rate).to eq(user_skill_rate.rate)
+        end
       end
     end
   end
