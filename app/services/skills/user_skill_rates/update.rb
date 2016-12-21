a08ce65a7420632af58d337ce2c3c5ca3c928d3f
Dariusz Musielak <dariusz.musielak@netguru.pl>
2016-12-21 18:49:06 +0100

    Use update instead of update_attributes

--- app/services/skills/user_skill_rates/update.rb
+++ app/services/skills/user_skill_rates/update.rb
@@ -17,12 +17,12 @@ module Skills
       attr_reader :user_skill_rate_id, :params
 
       def update_user_skill_rate
-        user_skill_rate.update_attributes(update_params)
+        user_skill_rate.update(update_params)
       end
 
       def create_or_update_user_skill_rate_content
         if user_skill_rate_content
-          user_skill_rate_content.update_attributes(update_params)
+          user_skill_rate_content.update(update_params)
         else
           user_skill_rate.contents.create(params)
         end
