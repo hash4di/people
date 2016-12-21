fc417ff34727bbd784627927495e0e84f8830808
Dariusz Musielak <dariusz.musielak@netguru.pl>
2016-12-21 18:48:28 +0100

    Scope in one line

--- app/models/user_skill_rate/content.rb
+++ app/models/user_skill_rate/content.rb
@@ -1,11 +1,7 @@
 class UserSkillRate::Content < ActiveRecord::Base
   belongs_to :user_skill_rate
 
-  scope :today, -> do
-    where(
-      'created_at > ?', Time.zone.now.beginning_of_day
-    )
-  end
+  scope :today, -> { where('created_at > ?', Time.zone.now.beginning_of_day) }
 
   validates :rate, presence: true
   validates_with ::UserSkillRate::RateWithinRangeValidator
