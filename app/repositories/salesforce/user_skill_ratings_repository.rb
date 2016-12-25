eed89013c9446714744b7f1500ac0df8eca5d507
Krzysztof Urban <krzysztof.urban@netguru.pl>
2016-12-25 20:53:21 +0100

    Reuse for other repositories

--- app/repositories/salesforce/user_skill_ratings_repository.rb
+++ app/repositories/salesforce/user_skill_ratings_repository.rb
@@ -1,46 +1,7 @@
 module Salesforce
   class UserSkillRatingsRepository
-    SALESFORCE_OBJECT_NAME = 'DeveloperSkillRating__c'.freeze
-
-    SyncFailed = Class.new(StandardError)
-
-    attr_reader :salesforce_client
-
-    def initialize(salesforce_client)
-      @salesforce_client = salesforce_client
-    end
-
-    def sync(rating)
-      if get_salesforce_id(rating)
-        update(rating)
-      else
-        create(rating)
-      end
-    end
-
-    private
-
-    def create(rating)
-      attrs = map_to_salesforce(rating)
-      salesforce_client.create(SALESFORCE_OBJECT_NAME, attrs).tap do |salesforce_id|
-        raise_sync_failed(rating) unless salesforce_id
-        rating.update_column(:salesforce_id, salesforce_id)
-      end.present?
-    end
-
-    def update(rating)
-      attrs = map_to_salesforce(rating).merge(Id: get_salesforce_id(rating))
-      salesforce_client.update(SALESFORCE_OBJECT_NAME, attrs).tap do |status|
-        raise_sync_failed(rating) unless status
-      end
-    end
-
-    def raise_sync_failed(rating)
-      fail SyncFailed, "couldn't sync UserSkillRating(id=#{ rating.id })"
-    end
-
-    def get_salesforce_id(rating)
-      rating.salesforce_id
+    def salesforce_object_name
+      'DeveloperSkillRating__c'
     end
 
     def map_to_salesforce(rating)
--- app/repositories/salesforce/users_repository.rb
+++ app/repositories/salesforce/users_repository.rb
@@ -1,46 +1,7 @@
 module Salesforce
-  class UsersRepository
-    SALESFORCE_OBJECT_NAME = 'Developer__c'.freeze
-
-    SyncFailed = Class.new(StandardError)
-
-    attr_reader :salesforce_client
-
-    def initialize(salesforce_client)
-      @salesforce_client = salesforce_client
-    end
-
-    def sync(user)
-      if get_salesforce_id(user)
-        update(user)
-      else
-        create(user)
-      end
-    end
-
-    private
-
-    def create(user)
-      attrs = map_to_salesforce(user)
-      salesforce_client.create(SALESFORCE_OBJECT_NAME, attrs).tap do |salesforce_id|
-        raise_sync_failed(user) unless salesforce_id
-        user.update_column(:salesforce_id, salesforce_id)
-      end.present?
-    end
-
-    def update(user)
-      attrs = map_to_salesforce(user).merge(Id: get_salesforce_id(user))
-      salesforce_client.update(SALESFORCE_OBJECT_NAME, attrs).tap do |status|
-        raise_sync_failed(user) unless status
-      end
-    end
-
-    def raise_sync_failed(user)
-      fail SyncFailed, "couldn't sync User(id=#{ user.id })"
-    end
-
-    def get_salesforce_id(user)
-      user.salesforce_id
+  class UsersRepository < GenericRepository
+    def salesforce_object_name
+      'Developer__c'
     end
 
     def map_to_salesforce(user)
