a4fc4c12e26916f3140e9faf49e2220299ee6931
Krzysztof Urban <krzysztof.urban@netguru.pl>
2016-12-25 20:06:32 +0100

    Begin generalization

--- app/repositories/salesforce/skills_repository.rb
+++ app/repositories/salesforce/skills_repository.rb
@@ -1,46 +1,7 @@
 module Salesforce
-  class SkillsRepository
-    SALESFORCE_OBJECT_NAME = 'DeveloperSkill__c'.freeze
-
-    SyncFailed = Class.new(StandardError)
-
-    attr_reader :salesforce_client
-
-    def initialize(salesforce_client)
-      @salesforce_client = salesforce_client
-    end
-
-    def sync(skill)
-      if get_salesforce_id(skill)
-        update(skill)
-      else
-        create(skill)
-      end
-    end
-
-    private
-
-    def create(skill)
-      attrs = map_to_salesforce(skill)
-      salesforce_client.create(SALESFORCE_OBJECT_NAME, attrs).tap do |salesforce_id|
-        raise_sync_failed(skill) unless salesforce_id
-        skill.update_column(:salesforce_id, salesforce_id)
-      end.present?
-    end
-
-    def update(skill)
-      attrs = map_to_salesforce(skill).merge(Id: get_salesforce_id(skill))
-      salesforce_client.update(SALESFORCE_OBJECT_NAME, attrs).tap do |status|
-        raise_sync_failed(skill) unless status
-      end
-    end
-
-    def raise_sync_failed(skill)
-      fail SyncFailed, "couldn't sync Skill(id=#{ skill.id })"
-    end
-
-    def get_salesforce_id(skill)
-      skill.salesforce_id
+  class SkillsRepository < GenericRepository
+    def salesforce_object_name
+      'DeveloperSkill__c'
     end
 
     def map_to_salesforce(skill)
