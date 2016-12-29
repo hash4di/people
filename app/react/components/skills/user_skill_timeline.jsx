e419792021d76d61bb721df6c97ffedc778e39e1
Tomasz Napieralski <tomasz.napieralski@netguru.co>
2016-12-29 18:27:01 +0100

    add labels to all months on timeline

--- app/react/components/skills/user_skill_timeline.jsx
+++ app/react/components/skills/user_skill_timeline.jsx
@@ -8,6 +8,7 @@ export default class UserSkillTimeline extends React.Component {
   minimumSVGwidth = 1000
   nextDays = 40
   previousDays = 40
+  labelFontSize = 14
 
   totalDays = null
   previousDaysWidth = null
@@ -142,17 +143,24 @@ export default class UserSkillTimeline extends React.Component {
     const startDate = Moment(nowDate).subtract(this.totalDays - this.nextDays, 'days');
     const endDate = Moment(nowDate).add(nextDays, 'days');
     const currentDate = Moment(startDate);
-    const separators = [];
+    const lines = [];
+    const labels = [];
     
     while (currentDate.diff(endDate, 'days') < 0) {
       currentDate.startOf('month').add(1, 'months');
       
       const positionX = currentDate.diff(startDate, 'days') * this.svgWidthScale;
-      separators.push(<line x1={positionX} y1="0%" x2={positionX} y2="100%" strokeWidth="2" stroke="black" />);
+      lines.push(<line x1={positionX} y1="0%" x2={positionX} y2="100%" strokeWidth="1" stroke="black" />);
+      labels.push(<text x={positionX + 10} y={this.labelFontSize + 10} fontFamily="Helvetica Neue" fontSize={this.labelFontSize}>{currentDate.format('MMMM Y')}</text>);
     }
 
+    const positionX = nowDate.diff(startDate, 'days') * this.svgWidthScale;
+    lines.push(<line x1={positionX} y1="0%" x2={positionX} y2="100%" strokeWidth="1" stroke="red" />);
+    labels.push(<text x={positionX + 10} y={(this.labelFontSize + 10) * 2} fontFamily="Helvetica Neue" fontSize={this.labelFontSize}>Today</text>);
+
     return <svg className={`${this.cssNamespace}__timeline-ui`} version="1.1" baseProfile="full" width={this.svgWidth} height={this.svgHeight} xmlns="http://www.w3.org/2000/svg">
-      {separators}
+      {lines}
+      {labels}
     </svg>
   }
 
@@ -172,16 +180,16 @@ export default class UserSkillTimeline extends React.Component {
       return accumulator.concat(<rect x={positionX} y={positionY} width={width} height={height} fill="red" />);
     }, []);
 
-    return <svg className={`${this.cssNamespace}__row-item`} version="1.1" baseProfile="full" width={this.svgWidth} height={this.svgHeight} xmlns="http://www.w3.org/2000/svg">
+    return <svg className={`${this.cssNamespace}__row-item ${this.cssNamespace}__skill-timeline`} version="1.1" baseProfile="full" width={this.svgWidth} height={this.svgHeight} xmlns="http://www.w3.org/2000/svg">
       {rectangles}
     </svg>
   }
 
   getSkillTimelines(skillData) {
+    const monthSeparatorSVG = this.createMonthSeparatorSVG();
     const skillTimelines = skillData.reduce((accumulator, data) => {
       return accumulator.concat(this.createSkillTimelineSVG(data));
     }, []);
-    const monthSeparatorSVG = this.createMonthSeparatorSVG();
 
     return <div className={`${this.cssNamespace}__timelines`}>{monthSeparatorSVG}{skillTimelines}</div>
   }
