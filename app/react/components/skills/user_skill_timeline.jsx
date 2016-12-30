90d56b7b35732a308b891a78ed72fd8fe4a0f2e9
Tomasz Napieralski <tomasz.napieralski@netguru.co>
2016-12-30 16:18:22 +0100

    add support for favourite skills

--- app/react/components/skills/user_skill_timeline.jsx
+++ app/react/components/skills/user_skill_timeline.jsx
@@ -8,7 +8,7 @@ export default class UserSkillTimeline extends React.Component {
   nextDays = 40
   previousDays = 40
   labelFontSize = 14
-  chartHeight = 100
+  chartHeight = 70
   chartPadding = 10
   chartStrokeWidth = 5
   gridLabelsHeight = 35
@@ -26,12 +26,10 @@ export default class UserSkillTimeline extends React.Component {
     this.nextDaysWidth = this.nextDays * this.svgWidthScale;
     this.previousDaysWidth = this.previousDays * this.svgWidthScale;
 
-    // this should be taken from model
-    const maximumDays = 30;
-    const maximumDaysWidth = maximumDays * this.svgWidthScale;
+    const maximumDaysWidth = this.model.meta.maximumDays * this.svgWidthScale;
     const requiredSVGwidth = this.previousDaysWidth + this.nextDaysWidth + maximumDaysWidth;
 
-    this.svgHeight = this.model.length * (this.chartHeight + this.chartPadding * 2) + this.gridLabelsHeight;
+    this.svgHeight = this.model.skills.length * (this.chartHeight + this.chartPadding * 2) + this.gridLabelsHeight;
     this.svgWidth = requiredSVGwidth > this.minimumSVGwidth ? requiredSVGwidth : this.minimumSVGwidth;
     this.totalDays = this.svgWidth / this. svgWidthScale;
   }
@@ -49,96 +47,125 @@ export default class UserSkillTimeline extends React.Component {
   }
 
   getModel(data) {
-    return [
-      {
-        skillName: 'ember',
-        totalDays: 23,
-        maxRate: 3,
-        updates: [
-          {
-            days: 5,
-            rate: 0
-          },
-          {
-            days: 10,
-            rate: 1
-          },
-          {
-            days: 5,
-            rate: 2
-          },
-          {
-            days: 3,
-            rate: 3
-          }
-        ]
+    return {
+      meta: {
+        maximumDays: 200
       },
-      {
-        skillName: 'react',
-        totalDays: 30,
-        maxRate: 3,
-        updates: [
-          {
-            days: 5,
-            rate: 0
-          },
-          {
-            days: 13,
-            rate: 1
-          },
-          {
-            days: 10,
-            rate: 2
-          },
-          {
-            days: 2,
-            rate: 3
-          }
-        ]
-      },
-      {
-        skillName: 'git',
-        totalDays: 20,
-        maxRate: 3,
-        updates: [
-          {
-            days: 5,
-            rate: 0
-          },
-          {
-            days: 7,
-            rate: 1
-          },
-          {
-            days: 5,
-            rate: 2
-          },
-          {
-            days: 3,
-            rate: 1
-          }
-        ]
-      },
-      {
-        skillName: 'jira',
-        totalDays: 10,
-        maxRate: 1,
-        updates: [
-          {
-            days: 2,
-            rate: 0
-          },
-          {
-            days: 8,
-            rate: 1
-          }
-        ]
-      }
-    ]
+      skills: [
+        {
+          skillName: 'ember',
+          totalDays: 100,
+          maxRate: 3,
+          updates: [
+            {
+              days: 40,
+              rate: 0,
+              isFavourite: false
+            },
+            {
+              days: 20,
+              rate: 1,
+              isFavourite: false
+            },
+            {
+              days: 10,
+              rate: 2,
+              isFavourite: false
+            },
+            {
+              days: 30,
+              rate: 3,
+              isFavourite: false
+            }
+          ]
+        },
+        {
+          skillName: 'react',
+          totalDays: 30,
+          maxRate: 3,
+          updates: [
+            {
+              days: 5,
+              rate: 0,
+              isFavourite: false
+            },
+            {
+              days: 13,
+              rate: 1,
+              isFavourite: false
+            },
+            {
+              days: 10,
+              rate: 2,
+              isFavourite: false
+            },
+            {
+              days: 2,
+              rate: 3,
+              isFavourite: false
+            }
+          ]
+        },
+        {
+          skillName: 'git',
+          totalDays: 200,
+          maxRate: 3,
+          updates: [
+            {
+              days: 50,
+              rate: 0,
+              isFavourite: false
+            },
+            {
+              days: 20,
+              rate: 1,
+              isFavourite: false
+            },
+            {
+              days: 30,
+              rate: 1,
+              isFavourite: true
+            },
+            {
+              days: 20,
+              rate: 1,
+              isFavourite: false
+            },
+            {
+              days: 50,
+              rate: 2,
+              isFavourite: true
+            },
+            {
+              days: 30,
+              rate: 1,
+              isFavourite: true
+            }
+          ]
+        },
+        {
+          skillName: 'jira',
+          totalDays: 100,
+          maxRate: 1,
+          updates: [
+            {
+              days: 20,
+              rate: 0,
+              isFavourite: false
+            },
+            {
+              days: 80,
+              rate: 1,
+              isFavourite: false
+            }
+          ]
+        }
+      ]
+    };
   }
 
   getSkillLabels() {
-    const skillLabels = this.model.reduce((acc, skillData) => {
+    const skillLabels = this.model.skills.reduce((acc, skillData) => {
       return acc.concat(<li className={`${this.cssNamespace}__labels-item`}>{skillData.skillName}</li>);
     }, []);
 
@@ -156,7 +183,7 @@ export default class UserSkillTimeline extends React.Component {
   }
 
   getTimelineBackground() {
-    const modelLenght = this.model.length;
+    const modelLenght = this.model.skills.length;
     const elements = [];
 
     for (let i = 0; i < modelLenght; ++i) {
@@ -173,7 +200,7 @@ export default class UserSkillTimeline extends React.Component {
   }
 
   getCharts() {
-    return this.model.reduce((acc, skillData, index) => {
+    return this.model.skills.reduce((acc, skillData, index) => {
       const offsetTop = (this.chartHeight + this.chartPadding * 2) * index + this.chartPadding + this.gridLabelsHeight;
       return acc.concat(this.getChart(skillData, this.chartHeight, offsetTop));
     }, []);
@@ -190,21 +217,22 @@ export default class UserSkillTimeline extends React.Component {
       const width = skillData.days * this.svgWidthScale;
       const positionY = chartHeight - height + offsetTop;
       const chartColor = this.getChartColor(skillData.rate, data.maxRate);
+      const strokeDasharray = skillData.isFavourite ? 'none' : '10, 10';
+      const chartStrokeWidth = skillData.isFavourite ? this.chartStrokeWidth : this.chartStrokeWidth / 2;
 
       const previousHorizontalLineProps = horizontalLines[horizontalLines.length - 1] &&
         horizontalLines[horizontalLines.length - 1].props ? horizontalLines[horizontalLines.length - 1].props : {};
-      const previousHorizontalLinePositionX = previousHorizontalLineProps.x1 ? previousHorizontalLineProps.x1 : offsetLeft;
-      const previousHorizontalLinePositionY = previousHorizontalLineProps.y1 ? previousHorizontalLineProps.y1 : 0;
-      const previousHorizontalLineWidth = previousHorizontalLineProps.x1 && previousHorizontalLineProps.x2 ?
-        previousHorizontalLineProps.x2 - previousHorizontalLineProps.x1 : 0;
+      const previousHorizontalLinePositionX = previousHorizontalLineProps.x1 || offsetLeft;
+      const previousHorizontalLinePositionY = previousHorizontalLineProps.y1 || 0;
+      const previousHorizontalLineWidth = previousHorizontalLineProps.x2 - previousHorizontalLineProps.x1 || 0;
       
       const positionX = previousHorizontalLinePositionX + previousHorizontalLineWidth;
-      horizontalLines.push(<line x1={positionX} y1={positionY} x2={positionX + width}
-        y2={positionY} strokeWidth={this.chartStrokeWidth} strokeLinecap="round" stroke={chartColor} />);
+      horizontalLines.push(<line x1={positionX} y1={positionY} x2={positionX + width} y2={positionY}
+        strokeWidth={chartStrokeWidth} strokeDasharray={strokeDasharray} stroke={chartColor} />);
       
       if (height !== 0) {
         verticalLines.push(<line x1={positionX} y1={previousHorizontalLinePositionY}
-          x2={positionX} y2={positionY} strokeWidth="1" strokeDasharray="1, 5" stroke="black" />);
+          x2={positionX} y2={positionY} strokeWidth="1" strokeDasharray="1, 6" stroke="black" />);
       }
     });
 
@@ -244,7 +272,7 @@ export default class UserSkillTimeline extends React.Component {
 
     // current day line with label
     const positionX = Moment().diff(startDate, 'days') * this.svgWidthScale;
-    elements.push(...this.getVerticalLineWithLabel(positionX, 'Today', 40, 5, "red", "red"));
+    elements.push(...this.getVerticalLineWithLabel(positionX, 'Today', 40, 10, "red", "red"));
 
     // horizontal line
     elements.push(<line x1="0" y1={this.gridLabelsHeight} x2={this.svgWidth} y2={this.gridLabelsHeight} strokeWidth="1" stroke="#d6dade" />);
