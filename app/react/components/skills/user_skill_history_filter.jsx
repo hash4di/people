62498064b5ed4ac317c8cea5111ed9fb02a8af37
Tomasz Napieralski <tomasz.napieralski@netguru.co>
2017-01-04 00:15:24 +0100

    add filter date binding

--- app/react/components/skills/user_skill_history_filter.jsx
+++ app/react/components/skills/user_skill_history_filter.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
+import Moment from 'moment';
 
 export default class UserSkillHistoryFilter extends React.Component {    
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.setDateRange(12, true);
+    }
+    
+    setDateRange(months, firstSet) {
+        const format = 'Y-MM-DD';
+        const fromDate = Moment().subtract(months, 'months').format(format);
+        const toDate = Moment().format(format);
+
+        if (firstSet) {
+            this.state.fromDate = fromDate;
+            this.state.toDate = toDate;
+        } else {
+            this.setState({fromDate, toDate});
+        }
+    }
+
+    onDateChange(dateInput, date) {
+        this.setState({[dateInput]: date});
+    }
+    
     render() {
         const {cssNamespace, listItems, listPrimaryText, onItemClick} = this.props;
         const listElements = listItems.reduce((acc, listItem, index) => {
@@ -11,9 +35,19 @@ export default class UserSkillHistoryFilter extends React.Component {
             );
         }, []);
 
-        return <ul className={`${cssNamespace} nav nav-tabs`}>
-            <li className="text-primary">{listPrimaryText}</li>
-            {listElements}
-        </ul>;
+        return <div className={cssNamespace}>
+            <ul className={`${cssNamespace} nav nav-tabs`}>
+                <li className="text-primary">{listPrimaryText}</li>
+                {listElements}
+            </ul>
+            <div>
+                <button onClick={() => {this.setDateRange(1);}}>last month</button>
+                <button onClick={() => {this.setDateRange(3);}}>last 3 months</button>
+                From:
+                <input type="date" value={this.state.fromDate} onChange={(event) => {this.onDateChange('fromDate', event.target.value);}} />
+                To:
+                <input type="date" value={this.state.toDate} onChange={(event) => {this.onDateChange('toDate', event.target.value);}} />
+            </div>
+        </div>;
     }
 }
