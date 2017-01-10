6185547d2fca98e222a5f707d95db518b1a5f870
Dariusz Musielak <dariusz.musielak@netguru.pl>
2017-01-10 20:14:45 +0100

    Require user_id to fetch data

--- app/controllers/api/v3/user_skill_rates_controller.rb
+++ app/controllers/api/v3/user_skill_rates_controller.rb
@@ -1,13 +1,14 @@
 module Api
   module V3
     class UserSkillRatesController < Api::ApiController
+      before_action :set_user, only: :index
+
       expose(:user_skill_rates) do
-        UserSkillRatesQuery.new(current_user).results_for_category(
+        UserSkillRatesQuery.new(@user).results_for_category(
           filter_params[:category]
         )
       end
 
-      # TODO: remove N+1 query fot skill and skill_categories
       def index
         render json: serialize_user_skill_rates, root: false
       end
@@ -16,15 +17,28 @@ module Api
 
       def serialize_user_skill_rates
         ActiveModel::ArraySerializer.new(
-          user_skill_rates, each_serializer: ContentsHistorySerializer, context: {
-           start_date: filter_params[:start_date]&.to_datetime,
-           end_date: filter_params[:end_date]&.to_datetime
-         }
-        ).to_json
+          user_skill_rates.includes(skill: :skill_category),
+          each_serializer: ContentsHistorySerializer,
+          context: serializer_context
+        )
+      end
+
+      def serializer_context
+        {
+          start_date: filter_params[:start_date]&.to_datetime,
+          end_date: filter_params[:end_date]&.to_datetime
+        }
       end
 
       def filter_params
-        params.permit(:category, :start_date, :end_date)
+        params.permit(:category, :start_date, :end_date, :user_id)
+      end
+
+      def set_user
+        @user ||= User.find_by_id(filter_params[:user_id])
+        if @user.blank?
+          render json: "Sorry, such user doesn't exist.", status: 404 and return
+        end
       end
     end
   end
