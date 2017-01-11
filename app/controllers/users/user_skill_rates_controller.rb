566a90e45136db570c6dde24b0c396434caac0ff
Dariusz Musielak <dariusz.musielak@netguru.pl>
2017-01-11 13:49:32 +0100

    expose skill categories for history view

--- app/controllers/users/user_skill_rates_controller.rb
+++ app/controllers/users/user_skill_rates_controller.rb
@@ -1,6 +1,7 @@
 module Users
   class UserSkillRatesController < ApplicationController
     expose(:user) { User.find_by_id(params[:id]) }
+    expose(:skill_categories) { SkillCategory.all }
 
     def history; end
   end
