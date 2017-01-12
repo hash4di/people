5bbc6fbd0c6649ba62e19f41dac312be69edc1ad
Janek Kamyk <jan.kamyk@netguru.pl>
2017-01-12 12:17:18 +0100

    Account for differences in user email domain

--- app/controllers/api/v2/user_skill_rates_controller.rb
+++ app/controllers/api/v2/user_skill_rates_controller.rb
@@ -1,8 +1,17 @@
 module Api::V2
   class UserSkillRatesController < Api::ApiController
     def index
-      user = User.find_by(email: params[:user_email])
+      user = find_user_by_email
       render json: user, serializer: UserSkillRatesSerializer
+    end
+
+    private
+
+    def find_user_by_email
+      email = params[:user_email][0...-2]
+      pl = email + 'pl'
+      co = email + 'co'
+      User.find_by(email: pl) || User.find_by(email: co)
     end
   end
 end
--- spec/controllers/api/v2/user_skill_rates_controller_spec.rb
+++ spec/controllers/api/v2/user_skill_rates_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Api::V2::UserSkillRatesController do
   describe 'GET #index' do
     let(:token) { AppConfig.api_token }
-    let(:user) { create :user }
+    let(:user) { create :user, email: 'john.smith@netguru.pl' }
     let(:skill) { create :skill }
     let(:unknown_email) { 'asdasd@netguru.pl' }
     let!(:user_skill_rate1) { create(:user_skill_rate, user: user, skill: skill, rate: 3) }
@@ -29,6 +29,18 @@ describe Api::V2::UserSkillRatesController do
 
     context 'with api token and valid user_email' do
       before { get :index, token: token, user_email: user.email }
+
+      it { expect(response.status).to eq(200) }
+
+      it 'returns correct hash', :aggregate_failures do
+        skill_rates = json_response['user_skill_rates']['skill_rates']
+        expect(skill_rates).to be_a(Array)
+        expect(skill_rates.size).to eq(2)
+      end
+    end
+
+    context 'with api token and user_email with different domain' do
+      before { get :index, token: token, user_email: 'john.smith@netguru.co' }
 
       it { expect(response.status).to eq(200) }
 
