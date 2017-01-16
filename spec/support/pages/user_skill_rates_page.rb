7bcf94e887653df5024970eabb49e247f1f383d7
Dorota Niewęgłowska <dorota.nieweglowska@o2.pl>
2017-01-16 19:38:49 +0100

    add elements to user skill rates page

--- spec/support/pages/user_skill_rates_page.rb
+++ spec/support/pages/user_skill_rates_page.rb
@@ -4,5 +4,6 @@ class UserSkillRatesPage < SitePrism::Page
   elements :skill_rate1, '.glyphicon.skill__rate[data-rate="1"]'
   elements :skill_rate2, '.glyphicon.skill__rate[data-rate="2"]'
   elements :skill_rate3, '.glyphicon.skill__rate[data-rate="3"]'
-  elements :skill__favorite, '.skill__favorite'
+  elements :skill_favorite, '.skill__favorite'
+  elements :clear_rate, '.skill__clear_rate'
 end
