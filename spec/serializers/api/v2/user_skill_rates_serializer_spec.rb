4018ca37c3b835e9c8328de2dc42863cbed141bc
Janek Kamyk <jan.kamyk@netguru.pl>
2017-01-16 15:23:22 +0100

    Fix UserSkillRateSerializer spec

--- spec/serializers/api/v2/user_skill_rates_serializer_spec.rb
+++ spec/serializers/api/v2/user_skill_rates_serializer_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 describe Api::V2::UserSkillRatesSerializer do
   subject { described_class.new(user_skill_rate1).serializable_hash }
-  let!(:user) { create(:user) }
-  let!(:skill) { create(:skill) }
-  let!(:user_skill_rate1) { create(:user_skill_rate, user: user, skill: skill, rate: 3) }
+  let(:user) { create(:user) }
+  let(:skill) { create(:skill) }
+  let(:user_skill_rate1) { create(:user_skill_rate, user: user, skill: skill, rate: 3) }
+  let(:expected_hash) do
+    { ref_name: skill.ref_name.to_s, rate: user_skill_rate1.rate }
+  end
 
   context 'returns user skill rates hash' do
     it 'returns correct hash', :aggregate_failures do
       expect(subject).to be_a(Hash)
-      expect(subject)
-        .to eq(
-          { ref_name: skill.ref_name.to_s, rate: user_skill_rate1.rate }
-        )
+      expect(subject).to eq(expected_hash)
     end
   end
 end
