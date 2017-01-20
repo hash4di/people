150ea22770c07d0675d4555699142f93a485da97
Dariusz Musielak <dariusz.musielak@netguru.pl>
2017-01-20 11:55:42 +0100

    hound fixes

--- spec/services/skills/generate_from_seeds_spec.rb
+++ spec/services/skills/generate_from_seeds_spec.rb
@@ -10,20 +10,28 @@ describe Skills::GenerateFromSeeds do
       before do
         SKILLS_AND_CATEGORIES = [
           {
-            :ref_name=>"backend_ruby-on-rails", :name=>"Ruby on Rails", :rating=>"int", :description=>"Rails is a web application development framework written in the Ruby language. It is designed to make programming web applications easier by making assumptions about what every developer needs to get started.", :category=>"backend"
+            ref_name: 'backend_ruby-on-rails',
+            name: 'Ruby on Rails',
+            rating:'int',
+            description: 'Rails is a web application development framework written in the Ruby language. It is designed to make programming web applications easier by making assumptions about what every developer needs to get started.',
+            category: 'backend'
           },
           {
-            :ref_name=>"backend_sinatra", :name=>"Sinatra", :rating=>"boolean", :description=>"Sinatra is a free and open source software web application library and domain-specific language written in Ruby. It is an alternative to other Ruby web application frameworks such as Ruby on Rails. It is dependent on the Rack web server interface.", :category=>"backend"
+            ref_name: 'backend_sinatra',
+            name: 'Sinatra',
+            rating: 'boolean',
+            description: 'Sinatra is a free and open source software web application library and domain-specific language written in Ruby. It is an alternative to other Ruby web application frameworks such as Ruby on Rails. It is dependent on the Rack web server interface.',
+            category: 'backend'
           }
         ]
       end
 
       it 'creates one new category' do
-        expect { subject }.to change{ SkillCategory.count }.from(0).to(1)
+        expect { subject }.to change { SkillCategory.count }.from(0).to(1)
       end
 
       it 'creates two new skills' do
-        expect { subject }.to change{ Skill.count }.from(0).to(2)
+        expect { subject }.to change { Skill.count }.from(0).to(2)
       end
 
       it 'returns correct message' do
@@ -35,7 +43,7 @@ describe Skills::GenerateFromSeeds do
       before { SKILLS_AND_CATEGORIES = [] }
 
       it 'raises error' do
-        expect{ subject }.to raise_error('Please run `rake skills:generate_seeds` first')
+        expect { subject }.to raise_error('Please run `rake skills:generate_seeds` first')
       end
     end
   end
