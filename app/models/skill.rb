8601ae0d9f97f02e983a77eb66fd5e53d58ae1f6
Janek Kamyk <jan.kamyk@netguru.pl>
2017-01-20 11:43:35 +0100

    Fix skill model validation

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -6,12 +6,16 @@ class Skill < ActiveRecord::Base
   validates :ref_name, :name, :skill_category, :rate_type, presence: true
   validates :ref_name, uniqueness: true
   validates :rate_type, inclusion: { in: ::Skills::RateType.stringified_types }
-  validate :uniques, if: :new_record?
+  validate :uniques, if: :new_record_or_uniques_update?
 
   private
 
   def uniques
     return unless self.class.where(name: name, skill_category_id: skill_category_id).exists?
     errors.add('name & skill_category', 'must be uniq')
+  end
+
+  def new_record_or_uniques_update?
+    new_record? || (persisted? && (name_changed? || skill_category_id_changed?))
   end
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Skill do
   it { is_expected.to belong_to :skill_category }
   it { is_expected.to have_many :users }
+  it { is_expected.to have_many :user_skill_rates }
 
   it { is_expected.to  validate_presence_of :ref_name }
   it { is_expected.to  validate_uniqueness_of :ref_name }
@@ -13,13 +14,37 @@ describe Skill do
 
   describe 'uniques validation' do
     let(:category) { create(:skill_category) }
+    let(:other_category) { create(:skill_category) }
 
-    before { create(:skill, name: 'foo', skill_category: category) }
+    let!(:existing_skill) do
+      create(:skill, name: 'foo', skill_category: category)
+    end
 
-    it 'ensurs uniques by name & category' do
+    it 'ensurs uniques by name & category for new_record' do
       skill = build(:skill, name: 'foo', skill_category: category)
       expect(skill.valid?).to be false
-      expect(skill.errors.messages).to eq :'name & skill_category' => ['must be uniq']
+      expect(skill.errors.messages)
+        .to eq :'name & skill_category' => ['must be uniq']
+    end
+
+    it 'ensurs uniques by name & category when skill is updated' do
+      persisted_skill = create(:skill, name: 'ROR', skill_category: category)
+      persisted_skill.update(name: 'foo')
+      expect(persisted_skill.valid?).to be false
+      expect(persisted_skill.errors.messages)
+        .to eq :'name & skill_category' => ['must be uniq']
+      persisted_skill.reload
+      persisted_skill.update(name: 'foo', skill_category: other_category)
+      expect(persisted_skill.valid?).to be true
+      persisted_skill.update(skill_category: category)
+      expect(persisted_skill.valid?).to be false
+      expect(persisted_skill.errors.messages)
+        .to eq :'name & skill_category' => ['must be uniq']
+    end
+
+    it 'allows update of other attributes' do
+      existing_skill.update(rate_type: 'boolean', description: 'description')
+      expect(existing_skill.valid?).to be true
     end
   end
 end
