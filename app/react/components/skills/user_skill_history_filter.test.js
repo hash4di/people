e6ab1e98dc8f6f8dbe8e6ce66f11cbaac0e51bda
Tomasz Napieralski <tomasz.napieralski@netguru.co>
2017-01-20 17:39:10 +0100

    make snapshots of component user_skill_history_filter

--- app/react/components/skills/user_skill_history_filter.test.js
+++ app/react/components/skills/user_skill_history_filter.test.js
@@ -1,36 +1,27 @@
 import React from 'react';
 import UserSkillHistoryFilter from './user_skill_history_filter';
 import {shallow} from 'enzyme';
+import renderer from 'react-test-renderer';
 
-test('render component without any parameters', () => {
-  const subject = shallow(<UserSkillHistoryFilter />);
-  expect(subject.html()).not.toBeNull();
-});
-
-test('component wrapper has expected structure', () => {
+test('component structure match snapshot', () => {
   const cssNamespace = 'test-subject';
-  const subject = shallow(
+  const subject = renderer.create(
     <UserSkillHistoryFilter cssNamespace={cssNamespace} />
   );
 
-  expect(subject.is('div')).toBeTruthy();
-  expect(subject.hasClass(cssNamespace)).toBeTruthy();
-  expect(subject.find('> div').length).toBe(2);
-
-  expect(subject
-    .find(`.${cssNamespace}__filter-category`)
-    .is('div')
-  ).toBeTruthy();
-
-  expect(subject
-    .find(`.${cssNamespace}__filter-date`)
-    .is('div')
-  ).toBeTruthy();
+  expect(subject.toJSON()).toMatchSnapshot();
 });
 
-test('category filter sub-component has expected structure', () => {
+test('component structure without any parameter match snapshot', () => {
+  const subject = renderer.create(<UserSkillHistoryFilter />);
+  expect(subject.toJSON()).toMatchSnapshot();
+});
+
+test('component structure with all parameters match snapshot', () => {
   const cssNamespace = 'test-subject';
-  const listPrimaryText = "My test list";
+  const listPrimaryText = 'My test list';
+  const startDate = '2016-12-01';
+  const endDate = '2016-12-12';
   const listItems = [
     {
       name: 'skill_1',
@@ -45,43 +36,16 @@ test('category filter sub-component has expected structure', () => {
       isActive: false
     }
   ];
-  const subject = shallow(
+  const component = (
     <UserSkillHistoryFilter
       cssNamespace={cssNamespace}
       listItems={listItems}
       listPrimaryText={listPrimaryText}
+      startDate={startDate}
+      endDate={endDate}
     />
   );
-
-  expect(subject
-    .find(`.${cssNamespace}__filter-category-primary-text`)
-    .is('div')
-  ).toBeTruthy();
-
-  expect(subject
-    .find(`.${cssNamespace}__filter-category-primary-text`)
-    .text()
-  ).toBe(listPrimaryText);
-
-  expect(subject
-    .find(`.${cssNamespace}__filter-category-list`)
-    .is('ul')
-  ).toBeTruthy();
-
-  expect(subject
-    .find(`.${cssNamespace}__filter-category-list`)
-    .hasClass('nav')
-  ).toBeTruthy();
-
-  expect(subject
-    .find(`.${cssNamespace}__filter-category-list`)
-    .hasClass('nav-tabs')
-  ).toBeTruthy();
-
-  expect(subject
-    .find(`.${cssNamespace}__filter-category-list-item`)
-    .length
-  ).toBe(listItems.length);
+  const subject = shallow(component);
 
   listItems.forEach(({name, isActive}, index) => {
     const listItem = subject
@@ -97,42 +61,7 @@ test('category filter sub-component has expected structure', () => {
         listItem.hasClass(`${cssNamespace}__filter-category-active-item`)
       ).not.toBeTruthy();
     }
-
-    expect(listItem.text()).toBe(name);
   });
-});
 
-test('category filter sub-component has expected structure', () => {
-  const cssNamespace = 'test-subject';
-  const startDate = "2016-10-10";
-  const endDate = "2016-12-12";
-  const subject = shallow(
-    <UserSkillHistoryFilter
-      cssNamespace={cssNamespace}
-      startDate={startDate}
-      endDate={endDate}
-    />
-  );
-  const filterNodes = subject.find(`.${cssNamespace}__filter-date`).children();
-
-  expect(filterNodes.length).toBe(6);
-  expect(filterNodes.at(0).is('button'));
-  expect(filterNodes.at(0).text()).toBe('last month');
-
-  expect(filterNodes.at(1).is('button'));
-  expect(filterNodes.at(1).text()).toBe('last 3 months');
-
-  expect(filterNodes.at(2).is('div'));
-  expect(filterNodes.at(2).text()).toBe('From:');
-
-  expect(filterNodes.at(3).is('input'));
-  expect(filterNodes.at(3).prop('type')).toBe('date');
-  expect(filterNodes.at(3).prop('value')).toBe(startDate);
-
-  expect(filterNodes.at(4).is('div'));
-  expect(filterNodes.at(4).text()).toBe('To:');
-
-  expect(filterNodes.at(5).is('input'));
-  expect(filterNodes.at(5).prop('type')).toBe('date');
-  expect(filterNodes.at(5).prop('value')).toBe(endDate);
+  expect(renderer.create(component).toJSON()).toMatchSnapshot();
 });
