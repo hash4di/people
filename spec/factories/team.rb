eee003a4dbac93a7490b1afb2129e7a98d1b53fa
Maciej Gąska <maciej.gaska@netguru.pl>
2017-01-31 14:42:57 +0100

    wut, forgot this one

--- spec/factories/team.rb
+++ spec/factories/team.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :team do
     name { Faker::Name.name }
 
-    factory :team_with_members
+    factory :team_with_members do
       transient do
         users_count 8
       end
@@ -10,5 +10,6 @@ FactoryGirl.define do
       after(:create) do |team, evaluator|
         create_list(:user, evaluator.users_count, teams: [team])
       end
+    end
   end
 end
