037bed7533fb01bf262e024f87e2ac2538aa784f
Maciej Gąska <maciej.gaska@netguru.pl>
2017-02-06 17:12:42 +0100

    add user skill rates view object

---
+++ app/view_objects/user_skill_rates_page.rb
@@ -0,0 +1,19 @@
+class UserSkillRatesPage
+  NO_PRIMARY_ROLE = :no_primary_role
+
+  def initialize(user:)
+    @user = user
+  end
+
+  def initial_skill_category(skill)
+    return 'js-initial-skill-category' if user_skill_category == skill
+  end
+
+  private
+
+  def user_skill_category
+    @user_skill_category ||= @user.primary_role&.skill_category ||
+                             @user.primary_roles.first ||
+                             NO_PRIMARY_ROLE
+  end
+end
