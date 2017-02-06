8b11366e07819ad3455752cdae11ae8389590fcb
Maciej Gąska <maciej.gaska@netguru.pl>
2017-02-06 14:26:36 +0100

    fix spec for skill category

--- spec/models/skill_category_spec.rb
+++ spec/models/skill_category_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe SkillCategory do
   it { is_expected.to have_many :skills }
-  it { is_expected.to have_and_belong_to_many :roles }
+  it { is_expected.to have_many :roles }
 
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_uniqueness_of :name }
