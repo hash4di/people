0a53b57167510f252e9d30e00882ed95b9e0836c
Dariusz Musielak <dariusz.musielak@netguru.pl>
2017-02-06 18:25:00 +0100

    Added relations between skill and draft skill

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,11 +2,17 @@ class Skill < ActiveRecord::Base
   belongs_to :skill_category
   has_many :user_skill_rates
   has_many :users, through: :user_skill_rates
+  has_many :draft_skill
+  has_one :requested_change, -> {
+    where(draft_type: 'update', draft_status: 'created')
+  }, anonymous_class: DraftSkill
 
   validates :ref_name, :name, :skill_category, :rate_type, presence: true
   validates :ref_name, uniqueness: true
   validates :rate_type, inclusion: { in: ::Skills::RateType.stringified_types }
   validate :uniques, if: :new_record_or_uniques_update?
+
+  attr_accessor :requester_explanation
 
   private
 
