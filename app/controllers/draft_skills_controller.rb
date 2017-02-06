e9de93cf1a8a7cf63b87ad703fa2e2ec356913b8
Dariusz Musielak <dariusz.musielak@netguru.pl>
2017-02-06 18:26:50 +0100

    Added draft skills controller

--- app/controllers/draft_skills_controller.rb
+++ app/controllers/draft_skills_controller.rb
@@ -1,2 +1,40 @@
+class DraftSkillsController < ApplicationController
   skip_before_filter :authenticate_admin!
   before_filter :authenticate_for_skills!
+  expose(:draft_skill) { DraftSkill.find(params[:id]) }
+  expose(:grouped_draft_skills_by_status) { grouped_draft_skills_by_status }
+  expose(:skill) { draft_skill.skill }
+  expose(:skill_categories) { SkillCategory.all }
+
+  def show; end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if DraftSkills::Update.new(draft_skill, draft_skill_params).call
+        format.html { redirect_to draft_skill.skill, notice: 'Skill was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: draft_skill.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def draft_skill_params
+    @draft_skill_params ||= begin
+      params.require(:draft_skill).permit(:reviewer_explanation, :draft_status).merge(
+        'reviewer_id' => current_user.id
+      )
+    end
+  end
+
+  def grouped_draft_skills_by_status
+    DraftSkill.where(
+      'created_at > ?', Time.now - 30.days
+    ).group_by(&:draft_status)
+  end
+end
