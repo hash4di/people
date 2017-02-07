436de6a5b0135cca650146ac4de885612a8cfa4d
Dariusz Musielak <dariusz.musielak@netguru.pl>
2017-02-07 21:21:10 +0100

    Modified service for creating draft_skill

--- app/services/draft_skills/create.rb
+++ app/services/draft_skills/create.rb
@@ -1,19 +1,42 @@
 module DraftSkills
   class Create
-    attr_reader :draft_skill_attributes
+    attr_reader :params, :type , :user, :skill, :draft_skill
 
-    def initialize(draft_skill_attributes)
-      @draft_skill_attributes = draft_skill_attributes
+    def initialize(params:, type: , user:, skill: nil)
+      @params = params
+      @type = type
+      @user = user
+      @skill = skill
+      initialize_draft_skill!
     end
 
-    def call
-      create
+    def save!
+      draft_skill.save
+    end
+
+    def valid?
+      draft_skill.valid?
+    end
+
+    def errors
+      draft_skill.errors
     end
 
     private
 
-    def create
-      DraftSkill.create(draft_skill_attributes) || false
+    def initialize_draft_skill!
+      @draft_skill ||= begin
+        draft_skill_attributes.merge!('skill_id' => skill.id) if skill
+        DraftSkill.new(draft_skill_attributes)
+      end
+    end
+
+    def draft_skill_attributes
+      @draft_skill_attributes ||= params.merge(
+        'requester_id' => user.id,
+        'draft_type' => type,
+        'draft_status' => 'created'
+      )
     end
   end
 end
