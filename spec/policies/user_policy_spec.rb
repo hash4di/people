0a339a3d2e2f4f8974725a35e5183e26a1597666
Dariusz Musielak <dariusz.musielak@netguru.pl>
2017-02-10 11:26:15 +0100

    Modified specs for user policy history access

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -1,13 +1,44 @@
 require 'spec_helper'
 
 describe UserPolicy do
-  subject { UserPolicy.new(leader, team_member) }
+  subject { described_class.new(current_user, user_with_history) }
+  let(:user_with_history) { create(:user) }
 
-  let(:leader) { create(:user) }
-  let!(:team) { create(:team_with_members, user_id: leader.id, users: [leader]) }
-  let(:team_member) { team.users.sample }
+  describe '#history?' do
+    context 'when it is user\'s history' do
+      let(:current_user) { create(:user) }
+      let(:user_with_history) { current_user }
 
-  before { team.reload }
+      it { expect(subject.history?).to be true }
+    end
 
-  it { expect(subject.history?).to be true }
+    context 'when user is a leader' do
+      let(:current_user) { create(:user, :leader) }
+
+      context 'when user want to access his team member' do
+        before do
+          current_user.teams.first.users << user_with_history
+        end
+        it { expect(subject.history?).to be true }
+      end
+
+      context 'when user want to access different team member' do
+        let(:different_leader) { create(:user, :leader) }
+        let!(:team) do
+          create(
+            :team_with_members,
+            user_id: different_leader.id,
+            users: [different_leader, user_with_history]
+          )
+        end
+
+        it { expect(subject.history?).to be false }
+      end
+    end
+
+    context 'when user is talent' do
+      let(:current_user) { create(:user, :talent) }
+      it { expect(subject.history?).to be true }
+    end
+  end
 end
