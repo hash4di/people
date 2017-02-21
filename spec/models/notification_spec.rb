f6f0085090bfc5bfba413d4ec02f51a37a447f34
Dariusz Musielak <dariusz.musielak@netguru.pl>
2017-02-21 10:40:48 +0100

    Added spec for notification model

---
+++ spec/models/notification_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Notification do
+  describe 'validations' do
+    let(:expected_STATUSES) { %w(notified unread) }
+    let(:expected_TYPES) { expected_SKILL_TYPES }
+    let(:expected_SKILL_TYPES) { %w(skill_updated skill_created) }
+
+    it { is_expected.to belong_to :notifiable }
+    it { is_expected.to belong_to :receiver }
+
+    it { is_expected.to validate_inclusion_of(:notification_type).in_array(expected_TYPES) }
+    it { is_expected.to validate_inclusion_of(:notification_status).in_array(expected_STATUSES) }
+  end
+
+  describe '#skill?' do
+    subject { notification.skill? }
+
+    let(:notification) { build(:notification) }
+    it { expect(subject).to eq true }
+  end
+
+  describe '#notified?' do
+    subject { notification.notified? }
+
+    context 'when notificaiton status equals unread' do
+      let(:notification) { build(:notification, :unread) }
+      it { expect(subject).to eq false }
+    end
+
+    context 'when notificaiton status equals notified' do
+      let(:notification) { build(:notification, :notified) }
+      it { expect(subject).to eq true }
+    end
+  end
+end
