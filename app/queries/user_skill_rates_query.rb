26ce03dbef2a0fe7ade04b32bb02a8d0f08814ab
Dariusz Musielak <dariusz.musielak@netguru.pl>
2017-02-23 11:25:33 +0100

    hound fixes

--- app/queries/user_skill_rates_query.rb
+++ app/queries/user_skill_rates_query.rb
@@ -6,7 +6,7 @@ class UserSkillRatesQuery
   end
 
   def results_by_categories
-    user_skill_rates.group_by(&:category).sort_by{ |key, _| key }.to_h
+    user_skill_rates.group_by(&:category).sort_by { |key, _| key }.to_h
   end
 
   def rated_skills
