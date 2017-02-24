4e18c854ee45498aa44e007d3ed1314099038c7d
Dariusz Musielak <dariusz.musielak@netguru.pl>
2017-02-24 16:11:14 +0100

    extracted seting skill_id to separate method, We need to do it since we work on draft_skill now

--- app/services/notifications/skill/updated.rb
+++ app/services/notifications/skill/updated.rb
@@ -11,10 +11,16 @@ module Notifications
         @receivers ||= User.joins(
           :user_skill_rates
         ).where(
-          user_skill_rates: { skill_id: notifiable_id}
+          user_skill_rates: { skill_id: skill_id }
         ).where.not(
           user_skill_rates: { rate: 0 }
         )
+      end
+
+      def skill_id
+        @skill_id ||= DraftSkill.where(
+          id: notifiable_id
+        ).select('skill_id').first.skill_id
       end
     end
   end
