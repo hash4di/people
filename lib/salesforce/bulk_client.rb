7aef0761f27a716c8b249943a186b1b69b2c1958
Maciej Gąska <maciej.gaska@netguru.pl>
2017-02-27 10:37:45 +0100

    create bulk client

---
+++ lib/salesforce/bulk_client.rb
@@ -0,0 +1,24 @@
+module Salesforce
+  class BulkClient
+    attr_reader :session_id, :server_url
+
+    def initialize
+      authorize!
+    end
+
+    def create_job(item)
+    end
+
+    def find_job(item)
+    end
+
+    def close_job(item)
+    end
+
+    private
+
+    def authorize!
+      @session_id, @server_url = Salesforce::Requests::Auth.new.connect
+    end
+  end
+end
