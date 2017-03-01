93ca425682fe7dc16424a83624ee188c6329f1c1
Dariusz Musielak <dariusz.musielak@netguru.pl>
2017-03-01 19:04:09 +0100

    Updated specs

--- spec/support/shared_examples/user_skill_rates_controller_shared.rb
+++ spec/support/shared_examples/user_skill_rates_controller_shared.rb
@@ -7,3 +7,13 @@ shared_examples 'returns correct hash and response is 200' do
     expect(skill_rates).to eq(expected_array)
   end
 end
+
+shared_examples 'returns empty hash and response is 200' do
+  it { expect(response.status).to eq(200) }
+
+  it 'returns empty hash', :aggregate_failures do
+    skill_rates = json_response['user_skill_rates']
+    expect(skill_rates).to be_a(Array)
+    expect(skill_rates.size).to eq(0)
+  end
+end
