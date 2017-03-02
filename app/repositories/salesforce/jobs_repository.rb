455fca5814f2720c0571028a6c1ad202b961d2da
Maciej Gąska <maciej.gaska@netguru.pl>
2017-03-02 13:04:53 +0100

    update item on success

--- app/repositories/salesforce/jobs_repository.rb
+++ app/repositories/salesforce/jobs_repository.rb
@@ -7,11 +7,18 @@ module Salesforce
     end
 
     def create(item)
-      salesforce_client.create(item)
+      salesforce_client.create_job(item) do |response|
+        item.update_attributes(
+          status: 'open',
+          salesforce_id: response[:id]
+        )
+      end
     end
 
-    def find(item)
-      salesforce_client.fetch_job(item)
+    def close(item)
+      if salesforce_client.close_job(item) do |_response|
+        item.update_attribute('state', 'closed')
+      end
     end
   end
 end
