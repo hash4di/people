a827eab73af1dbe383db8b363e09a0f75775a6d8
Maciej Gąska <maciej.gaska@netguru.pl>
2017-03-02 08:53:00 +0100

    validate data type

--- app/models/salesforce_job.rb
+++ app/models/salesforce_job.rb
@@ -1,4 +1,4 @@
 class SalesforceJob < ActiveRecord::Base
   validates :operation, inclusion: { in: %w(delete insert upsert update) }
-
+  validates :content_type, inclusion: { in: %w(CSV JSON XML) }
 end
