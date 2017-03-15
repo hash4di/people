90bcba39462b0e7cbb05e23ab81e570b5fd8b456
Maciej Gąska <maciej.gaska@netguru.pl>
2017-03-15 21:09:02 +0100

    add serializer for skill

--- app/serializers/salesforce/user_skill_rate_serializer.rb
+++ app/serializers/salesforce/user_skill_rate_serializer.rb
@@ -1,4 +1,27 @@
 module Salesforce
-  class UserSkillRateSerializer < ActiveModel::Serializers
+  class UserSkillRateSerializer < ActiveModel::Serializer
+    attributes :Contact__c, :DeveloperSkill__c, :Favorite__c, :Note__c, :Rating__c
+
+    self.root = false
+
+    def Contact__c
+      object.user.salesforce_id
+    end
+
+    def DeveloperSkill__c
+      object.skill.salesforce_id
+    end
+
+    def Favorite__c
+      object.favorite
+    end
+
+    def Note__c
+      object.note
+    end
+
+    def Rating__c
+      object.rate
+    end
   end
 end
