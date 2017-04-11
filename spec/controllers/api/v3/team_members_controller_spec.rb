a1332c2f1f0e176fa9deb76f476c8d232dc7b2c8
Dariusz Musielak <dariusz.musielak@netguru.co>
2017-04-11 13:17:55 +0200

    Added specs to team members controller in api v3

---
+++ spec/controllers/api/v3/team_members_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Api::V3::TeamMembersController do
+  describe 'GET #index' do
+    subject { get :index, params }
+
+    let!(:team) do
+      create(
+        :team,
+        name: 'alpha',
+        users: [team_member_1, team_member_2]
+      )
+    end
+    let(:team_member_1) { create(:user) }
+    let(:team_member_2) { create(:user) }
+
+    let(:token) { AppConfig.api_token }
+    let(:params) do
+      {
+        token: token,
+        team_name: 'alpha',
+      }
+    end
+
+    before { subject }
+    it { expect(response.status).to eq(200) }
+
+    it 'returns correct values' do
+      expect(json_response).to be_kind_of(Array)
+      expect(json_response.length).to be(2)
+      expect(json_response.first.keys).to contain_exactly(
+        'id', 'email', 'full_name'
+      )
+    end
+  end
+end
