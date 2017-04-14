557eb2ad4fb0a4a7e3acc4b580277981a1ff66f1
Dorota Niewęgłowska <dorota.nieweglowska@o2.pl>
2017-04-14 15:29:20 +0200

    spec for edit skill
    [PEOPLE-180]

--- spec/features/skills_spec.rb
+++ spec/features/skills_spec.rb
@@ -4,6 +4,8 @@ describe 'Skills page', js: true do
   let(:skills_page) { App.new.skills_page }
   let(:skills_new_page) { App.new.skills_new_page }
   let(:draft_skills_page) { App.new.draft_skills_page }
+  let(:skill_edit_page) { App.new.skill_edit_page }
+
 
   let(:skill_category_1) { create :skill_category, name: 'backend' }
   let(:skill_category_2) { create :skill_category, name: 'ios' }
@@ -33,5 +35,16 @@ describe 'Skills page', js: true do
     skills_page.add_new_skill.click
     skills_new_page.create_skill.click
     expect(skills_page).to have_content 'Skill category and skill name have to be set.'
+  end
+
+  it 'edits skill' do
+    skills_page.edit_skill.first.click
+    skill_edit_page.skill_name.set "test 2 test"
+    skill_edit_page.skill_description.set 'test test test'
+    skill_edit_page.skill_rate_type.select 'range'
+    skill_edit_page.skill_category.select 'backend'
+    skill_edit_page.requester_explanation.set 'test test test'
+    skill_edit_page.create_skill.click
+    expect(draft_skills_page).to have_content I18n.t('skills.message.update.success')
   end
 end
