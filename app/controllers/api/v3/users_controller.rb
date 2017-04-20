4013cb584e1ca37523db8636c473557cfe603f53
Dariusz Musielak <dariusz.musielak@netguru.co>
2017-04-20 08:45:40 +0200

    Added sing_in endpoint in users controller in api v3

--- app/controllers/api/v3/users_controller.rb
+++ app/controllers/api/v3/users_controller.rb
@@ -2,9 +2,24 @@ module Api
   module V3
     class UsersController < Api::V3::BaseController
       expose(:technical_users) { ScheduledUsersRepository.new.all }
+      skip_before_filter :authenticate_api!, only: [:sign_in]
 
       def technical
         render json: technical_users, each_serializer: Api::V3::UserWithMembershipsSerializer, root: false
+      end
+
+      def sign_in
+        user_exist = User.where(
+          email: user_params[:email],
+          api_token: user_params[:api_token]
+        ).active.exists?
+        user_exist ? authorized! : unauthorized!
+      end
+
+      private
+
+      def user_params
+        params.permit(:email, :api_token)
       end
     end
   end
