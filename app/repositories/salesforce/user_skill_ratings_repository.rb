f3fd55fc546b3d0f5f6b2f784d79bf24027ac75e
Krzysztof Kawa <krzysztofk131@gmail.com>
2017-05-23 15:43:57 +0200

    auto sync SkillRates with salesforce on any change

--- app/repositories/salesforce/user_skill_ratings_repository.rb
+++ app/repositories/salesforce/user_skill_ratings_repository.rb
@@ -1,5 +1,14 @@
 module Salesforce
   class UserSkillRatingsRepository < GenericRepository
+    def update(item)
+      attrs = map_to_salesforce(item)
+              .except(:Contact__c, :Skill__c)
+              .merge(Id: get_salesforce_id(item))
+      salesforce_client.update(salesforce_object_name, attrs).tap do |status|
+        raise_sync_failed(item) unless status
+      end
+    end
+
     def salesforce_object_name
       'SkillRating__c'
     end
