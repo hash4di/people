b10689a48dd2003095138acd27893e42b6fef155
Dorota Niewęgłowska <dorota.nieweglowska@o2.pl>
2017-08-21 12:36:22 +0200

    spec for Scheduling to rotate page
    [PEOPLE-212]

---
+++ spec/features/scheduling_to_rotate_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'Scheduling to rotate page', js: true do
+  let(:admin_user) { create(:user, :admin) }
+  let!(:developer) { create(:user, :developer)}
+  let!(:project) { create(:project, starts_at: DateTime.now, end_at: nil) }
+  let!(:current_membership_for_developer) do
+    create :membership,
+            starts_at: DateTime.current - 9.months,
+            ends_at: nil,
+            user: developer,
+            project: project
+  end
+  let!(:scheduling_to_rotate_page) { App.new.scheduling_to_rotate_page }
+
+  before do
+    log_in_as admin_user
+    scheduling_to_rotate_page.load
+  end
+
+  it 'displays technical users to rotate' do
+    expect(page).to have_content developer.last_name
+  end
+end
