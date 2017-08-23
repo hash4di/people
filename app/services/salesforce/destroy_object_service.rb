bf52cf7c91524f1d0cfc218bc7e2b30fb45736d2
Jedrzej Bryll <jedrzej.bryll@netguru.co>
2017-08-23 11:57:07 +0200

    Print error message to console

--- app/services/salesforce/destroy_object_service.rb
+++ app/services/salesforce/destroy_object_service.rb
@@ -27,9 +27,14 @@ module Salesforce
       @slack_notifier ||= SlackNotifier.new(channel: SLACK_CHANNEL)
     end
 
+    def notification_message(error_params)
+      @notification_message ||= NotificationMessage::UnableToDelete.new(error_params)
+    end
+
     def send_notifications(error_params)
       rollbar_notification(error_params)
       slack_notification(error_params)
+      console_notification(error_params)
     end
 
     def rollbar_notification(error_params)
@@ -41,7 +46,11 @@ module Salesforce
     end
 
     def slack_notification(error_params)
-      slack_notifier.ping(NotificationMessage::UnableToDelete.new(error_params))
+      slack_notifier.ping(notification_message(error_params))
+    end
+
+    def console_notification(error_params)
+      puts notification_message(error_params).message
     end
   end
 end
