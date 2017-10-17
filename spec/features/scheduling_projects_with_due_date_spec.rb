e2d50c2642a6e4479f3bfad2a39d3df45cecc937
Dorota Niewęgłowska <dorota.nieweglowska@o2.pl>
2017-10-17 15:34:54 +0200

    spec for scheduling In commercial project with due date page
    [PEOPLE-212]

---
+++ spec/features/scheduling_projects_with_due_date_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'Scheduling in commercial projects with due date page', js: true do
+  let(:admin_user) { create(:user, :admin) }
+  let!(:developer) { create(:user, :developer)}
+  let!(:project) { create(:project) }
+  let!(:current_membership_for_developer) do
+    create :membership,
+            starts_at: DateTime.current - 2.months,
+            ends_at: DateTime.current,
+            user: developer,
+            project: project
+  end
+  let!(:scheduling_projects_with_due_date_page) { App.new.scheduling_projects_with_due_date_page}
+
+  before do
+    log_in_as admin_user
+    scheduling_projects_with_due_date_page.load
+  end
+
+  it 'displays technical users in commercial projects with due date' do
+    expect(page).to have_content developer.last_name
+  end
+end
