90046914f6a1258022acbc79248ea3392b807d16
Dorota Niewęgłowska <dorota.nieweglowska@o2.pl>
2017-10-20 23:04:33 +0200

    add the new user row section to scheduling in rotations page
    [PEOPLE-212]

--- spec/support/pages/scheduling_in_rotations_page.rb
+++ spec/support/pages/scheduling_in_rotations_page.rb
@@ -1,3 +1,5 @@
 class SchedulingInRotationsPage < SitePrism::Page
   set_url '/scheduling/with_rotations_in_progress'
+
+  sections :user_rows, UserRowSection, '.scheduled-users tbody tr'
 end
--- spec/support/sections/user_row_section.rb
+++ spec/support/sections/user_row_section.rb
@@ -3,9 +3,11 @@ class UserRowSection < SitePrism::Section
     element :name, '.name'
   end
   section :current_project, '.projects-region' do
+    element :name, '.project-name'
     element :internal_label, '.project-label', text: 'INTERNAL'
   end
   section :next_project, '.next_projects-region' do
+    element :name, '.project-name'
     element :internal_label, '.project-label', text: 'INTERNAL'
   end
   section :booked_project, '.booked_projects-region' do
