c76ee423f7de99a8976d869420e083aa58ec911d
Dorota Niewęgłowska <dorota.nieweglowska@o2.pl>
2017-10-20 23:14:54 +0200

    spec for displaying projects in the scheduling in rotations page
    [PEOPLE-212]

--- spec/features/scheduling_in_rotations_spec.rb
+++ spec/features/scheduling_in_rotations_spec.rb
@@ -20,6 +20,7 @@ describe 'Scheduling in rotations page', js: true do
             project: project_2
   end
   let!(:scheduling_in_rotations_page) { App.new.scheduling_in_rotations_page }
+  let(:user_row) { scheduling_in_rotations_page.user_rows.first }
 
   before do
     log_in_as admin_user
@@ -27,6 +28,11 @@ describe 'Scheduling in rotations page', js: true do
   end
 
   it 'displays technical users in rotation in progress' do
-    expect(page).to have_content developer.last_name
+    expect(user_row.profile.name).to have_content developer.last_name
+  end
+
+  it 'displays the current project and the next project in the correct columns' do
+    expect(user_row.current_project.name).to have_link project_1.name
+    expect(user_row.next_project.name).to have_link project_2.name
   end
 end
