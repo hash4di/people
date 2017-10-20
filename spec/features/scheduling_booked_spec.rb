0d16a1ce8baba4dcafb6784ba5eac7c9fca14abf
Dorota Niewęgłowska <dorota.nieweglowska@o2.pl>
2017-10-20 16:11:19 +0200

    use the new user row section in scheduling booked specs
    [PEOPLE-212]

--- spec/features/scheduling_booked_spec.rb
+++ spec/features/scheduling_booked_spec.rb
@@ -13,6 +13,7 @@ describe 'Scheduling booked page', js: true do
             project: project
   end
   let!(:scheduling_booked_page) { App.new.scheduling_booked_page }
+  let(:user_row) { scheduling_booked_page.user_rows.first }
 
   before do
     log_in_as admin_user
@@ -20,7 +21,10 @@ describe 'Scheduling booked page', js: true do
   end
 
   it 'displays booked technical users' do
-    expect(page).to have_content developer.last_name
-    expect(scheduling_booked_page.user_rows.first.booking_projects).to have_link project.name
+    expect(user_row.profile.name).to have_content developer.last_name
+  end
+
+  it 'displays a project in the booked column' do
+    expect(user_row.booked_project).to have_link project.name
   end
 end
--- spec/support/pages/scheduling_booked_page.rb
+++ spec/support/pages/scheduling_booked_page.rb
@@ -1,7 +1,5 @@
 class SchedulingBookedPage < SitePrism::Page
   set_url '/scheduling/booked'
 
-  sections :user_rows, '.scheduled-users tbody tr' do
-    element :booking_projects, '.booked_projects-region'
-  end
+  sections :user_rows, UserRowSection, '.scheduled-users tbody tr'
 end
